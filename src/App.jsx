0930eb000b6cd7f60cc43ca62e3826f27088bfb6
Aghakarim-23 <hemidzade1010@gmail.com>
2026-04-10 03:07:21 +0400

    add: components & pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
+import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
